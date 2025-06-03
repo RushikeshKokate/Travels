4c512af0612eb360777b28057b96e43bb4a5cb5a
RushikeshKokate22 <rushikesh.kokate@tradebrains.in>
2025-06-03 23:06:28 +0530

    feat: download and set up fonts and images, install React Router

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "../assets/images/Screenshot (513).png"
+
+const Header = () => {
+  return (
+    <div>
+        <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default Header
